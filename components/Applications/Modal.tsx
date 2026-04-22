bf95b7f19f7498f06c8e73ce809384aaab187c25
jacobo <jacobohernandezmendieta7@gmail.com.com>
2026-04-22 14:10:54 -0600

    https app validator

--- components/Applications/Modal.tsx
+++ components/Applications/Modal.tsx
@@ -128,7 +128,7 @@ function CreateApplicationModal({ isOpen, onClose, appData, selectedGrants, list
   const isValidUrl = (url: string): boolean => {
     try {
       const urlObj = new URL(url)
-      return urlObj.protocol === "http:" /* urlObj.protocol === "http:"*/
+      return urlObj.protocol === "https:" /* urlObj.protocol === "http:"*/
     } catch {
       return false
     }
